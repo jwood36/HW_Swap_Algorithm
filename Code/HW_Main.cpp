a8dc2d47162c54a8a1bf1a158d6e9d724766c5d6
jwood36 <30936103+waaadmin@users.noreply.github.com>
2022-03-03 16:17:37 -0600

    Initial Commit

---
+++ Code/HW_Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+// Variable - START
+//	define variables that are global between the two Variable markers
+
+// Variable - END
+
+// Function/Method Declaration - START
+//	define function/method declaration signatures between the two markers
+
+// Function/Method Declaration - END
+
+int main() {
+	cout << "Hello World!";
+}
+
+// Function/Method Definition - START
+//	define function/method declaration signatures between the two markers
+
+// Function/Method Definition - END
